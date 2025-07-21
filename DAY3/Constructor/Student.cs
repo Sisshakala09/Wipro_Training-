2999d9dccc71cf08eb9e389eb026bb0e623b4ad7
Sisshakala09 <siddhakalagodase@gmail.com>
2025-07-21 18:53:18 +0530

    first commit

---
+++ DAY3/Constructor/Student.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace StudentProgram;  // Package
+class Student
+{
+
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int[] SubjectMarks { get; set; }
+
+
+}
+
+
+
